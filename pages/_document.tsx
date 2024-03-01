0107eb7a0440327e9c67bec81ff755e90a5a3ac5
xxx <xnatthawut1990@gmail.com>
2024-03-01 19:30:32 +0700

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+   <>
+    <Html lang="en">
+
+<link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+  <Head />
+  <body>
+    <Main />
+    <NextScript />
+  </body>
+  <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</Html>
+   
+   </>
+  );
+}
